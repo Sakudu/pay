a98aec528c23d0e3775d9927120fcb0c32cc5199
saku <antinomy_bruno@163.com>
2023-11-10 09:22:33 +0800

    incomplete

--- src/main/java/org/example/PayApplication.java
+++ src/main/java/org/example/PayApplication.java
@@ -2,11 +2,15 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author gwd
  * @date 2023/11/2 16:06
  */
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class PayApplication {
     public static void main(String[] args) {
